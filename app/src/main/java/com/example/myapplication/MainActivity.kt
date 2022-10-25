67d3d92b74e9ae719b26f34e7d4c959e308272f0
jeremiJD <jeremidjerem@gmail.com>
2022-10-25 10:24:19 +0200

    koncowy

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dopisany = findViewById<TextView>(R.id.dopisanytxt)
+        val nadbtn = findViewById<Button>(R.id.button5)
+        val dopbutton = findViewById<Button>(R.id.button3)
+        val input = findViewById<EditText>(R.id.wpisany)
+
+        dopbutton.setOnClickListener{
+            dopisany.text = dopisany.text.toString() + input.text.toString()
+
+        }
+        nadbtn.setOnClickListener {
+            dopisany.text = input.text.toString()
+        }
     }
+
+
+
+
+
 }
